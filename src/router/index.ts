571ece45a9ae6541998f3a144eca700324b2b5ad
Sozoeva <sozoeva.alina@gmail.com>
2024-05-27 13:28:06 +0600

    init HomePage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
     // {
     //   path: '/about',
     //   name: 'about',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import hero_bg from "../assets/images/hero_bg.png"
+</script>
+
 <template>
-  <h1>text</h1>
+<main>
+<div>
+<img :src="hero_bg" alt="hero_bg" class=" absolute">
+</div>
+<div>
+
+</div>
+</main>
 </template>
+
+<style scoped></style>
